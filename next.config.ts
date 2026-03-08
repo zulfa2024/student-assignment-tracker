bb9d2bc27b6b604ceeab82a6df29554002f506aa
zulfa2024 <zulfaoumer1@gmail.com>
2026-03-08 13:16:22 -0700

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    typedRoutes: false,
-  },
+  typedRoutes: false,
 };
 
 export default nextConfig;
