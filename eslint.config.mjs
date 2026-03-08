94958d904b78b6c90b07991a4b09514ebd56f581
zulfa2024 <zulfaoumer1@gmail.com>
2026-03-08 13:34:50 -0700

    Fix ESLint config for Next.js 15

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,9 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import next from "eslint-config-next";
+import { defineConfig } from "eslint/config";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+export default defineConfig([
+  {
+    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
+  },
+  ...next(),
 ]);
-
-export default eslintConfig;
